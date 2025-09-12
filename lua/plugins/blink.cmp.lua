9537616c5d14b8713c2fd337d2bef2f2a9c39bc8
MSA611 <shoaib9542186823@gmail.com>
2025-09-12 19:21:38 +0530

    removed lsp sync = true in blink.cmp.lua

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -12,7 +12,7 @@ return {
 			nerd_font_variant = "mono",
 		},
 
-		completion = { documentation = { auto_show = true }, lsp = { async = true } },
+		completion = { documentation = { auto_show = true } },
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
